1732c71f8803721719015a2e66e4d795b07cfdb1
Grigory Glushko <grinderrz@gmail.com>
2018-05-29 10:51:51 +0300

    put! -> !>>

--- src/ring/sse.clj
+++ src/ring/sse.clj
@@ -88,7 +88,7 @@
                 (if (map? event)
                   (reduce (fn [agg [k v]] (assoc agg k (str v))) {} event)
                   {:data (str event)})]
-            (when (send-event response-channel event-name event-data event-id async/put! raise)
+            (when (send-event response-channel event-name event-data event-id raise)
               (recur))))))
     (async/close! event-channel)
     (async/close! response-channel)
